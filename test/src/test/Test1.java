1952bda0d8bfe6e8904d9d5577fa2ba4e02dd9fe
LonelyPatients-qingwu <61537794+LonelyPatients-qingwu@users.noreply.github.com>
2020-03-07 16:23:31 +0800

    1

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("舍得让你孤独的人，又会在意你几分");
 	
 	System.out.println("不以物喜，不以己悲。");
+	
+	System.out.println("失去的，总是最美好的，例如回忆。看着镜子里面无表情的自己，无动于衷就连当个替代品都那么肆无忌惮的快乐是谁曾说过，会永远爱我。相爱没有那么容易，每个人有他的脾气。谁都不要轻易许下永远，谁都不能保证永远有多远。");
 }
 }
